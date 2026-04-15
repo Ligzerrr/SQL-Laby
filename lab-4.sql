feea69c4e1a053d77ccb36ba86e0292ed98a2ea3
Piotr <ligzer123@gmail.com>
2026-04-15 16:15:19 +0200

    Create lab-4.sql

---
+++ lab-4.sql
@@ -0,0 +1,37 @@
+-- =============================================
+-- Piotr
+-- Popiel
+-- 240164
+-- =============================================
+/* X to P | Y to 240164 | N to 4 */
+-- =============================================
+-- =============================================
+-- Zadanie 1
+DECLARE @Litera char(1);
+DECLARE @Cyfra int;
+SET @Cyfra = 4;
+SET @Litera = 'P';
+SELECT CustomerID, FirstName, LastName
+FROM SalesLT.Customer
+WHERE LastName Like @Litera + '%' AND CustomerID % 10 = @Cyfra;
+
+-- =============================================
+-- =============================================
+-- Zadanie 2
+DECLARE @Produkty TABLE (
+ProductID int,
+Name nvarchar(50),
+ListPrice money);
+INSERT INTO @Produkty(ProductID, Name, ListPrice) 
+SELECT ProductID, Name, ListPrice From SalesLT.Product WHERE NAME LIKE '%P%'
+SELECT *
+FROM @Produkty
+-- =============================================
+-- =============================================
+-- Zadanie 3
+SELECT cadrs.CustomerID, cadrs.FirstName, cadrs.LastName, adrs.City
+INTO #KlienciMiasta
+FROM SalesLT.CustomerAddress cadrs
+JOIN SalesLT.Address adrs on cadrs.AddressID = adrs.AddressID
+WHERE adrs.City = 
+-- =============================================
