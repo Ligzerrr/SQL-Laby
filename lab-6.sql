8cabc58d0f5ecbd4198855655467d215e32a4854
Piotr <ligzer123@gmail.com>
2026-04-16 21:32:55 +0200

    Create lab-6.sql

---
+++ lab-6.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Piotr
+-- Popiel
+-- 240164
+-- =============================================
+/* X to P | Y to 240164 | N to 4 */
+-- =============================================
+-- =============================================
+-- Zadanie 1
+
+BEGIN TRAN
+
+UPDATE SalesLT.Product WITH (TABLOCKX)
+SET ListPrice = ListPrice * 10
+SELECT * FROM SalesLT.Product --Nie miałem pomysłu jak inaczej dojść do blokady tabeli, gdy próbowałem 
+SELECT 
+    resource_type,
+    request_mode,
+    request_status
+FROM sys.dm_tran_locks;
+ROLLBACK TRAN
+-- =============================================
