ff3445fed3c95fd5950d62f68d443bfa887291c7
Piotr <ligzer123@gmail.com>
2026-04-17 12:26:49 +0200

    Update lab-6.sql

--- lab-6.sql
+++ lab-6.sql
@@ -7,11 +7,15 @@
 -- =============================================
 -- =============================================
 -- Zadanie 1
-
 BEGIN TRAN
 SELECT * FROM SalesLT.Product WITH (Tablockx) --TABLOCKX powoduje Blokade całej tabeli, Wiersze oraz czasami stronę można zablokować poprzez zrobienie zbyt dużych zmian typu update, ale TABLOCKX jest najprostszym i najlepszym sposobem zablokowania całej tabeli.
 -- Warto zaznaczyć, że z tej samej sesji zmiany przechodzą, ale np. inny użytkownik w sesji 2 musiałby poczekać aż tran zostanie commitowana lub rollbacknięta.
 --Blokowanie tabeli jest niebezpieczne, wszystkie inne sesje nie mają wpływu na dane, a wszystko  pozostaję zależne od sesji, która zablokowała tabelę. Na dodatek im dłuższy lock tym większa szansa na timeout, który niesie ze sobą dużo problemów np. cancel query, (bardzo źle...)
 --Blokowanie tabeli jest mniej optymalne, niż blokowanie samych wierszy - gdy np chcemy sie upewnic, że ktoś nie zmodyfikuję wierszy (na których pracujemy), ale dalej pozwala to na dodawanie i odczyt reszty tabeli!
 ROLLBACK TRAN
+-- =============================================
+-- =============================================
+-- Zadanie 2
+
+
 -- =============================================
