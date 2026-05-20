31c6554d1f613ca845f651b5b6b832e6adde6d0b
Piotr <ligzer123@gmail.com>
2026-05-21 00:46:13 +0200

    Update lab-7.sql

--- lab-7.sql
+++ lab-7.sql
@@ -12,8 +12,14 @@ GO
 
 ALTER TABLE [240164].[Customer] --To chyba jedyna tabela ktora zawiera LastName, chyba, że coś przeoczyłem ze zmęczenia.
 ALTER COLUMN LastName P4_surname
+GO
 -- =============================================
 -- =============================================
 -- Zadanie 2
 --Tutaj deklaruje zmienna jsonowa
 DECLARE @ProductInfo NVARCHAR(MAX) = N'[{"ProductID":717, "NewPrice": 1730.13},{"ProductID":718, "NewPrice": 2003.30},{"ProductID": 720, "NewPrice": 6001.34},{"ProductID": 730, "NewPrice": 50.31},{"ProductID": 740, "NewPrice": 705.50}]'
+
+--Tworze widok z jakas tam nazwa 
+CREATE VIEW dbo.JSONCOS AS
+SELECT
+	p.ProductID,
