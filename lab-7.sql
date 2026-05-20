8118e4db5b4704f0e067c358bc93be71536db5f2
Piotr <ligzer123@gmail.com>
2026-05-21 00:37:28 +0200

    Update lab-7.sql

--- lab-7.sql
+++ lab-7.sql
@@ -1,4 +1,4 @@
--- =============================================
+﻿-- =============================================
 -- Piotr
 -- Popiel
 -- 240164
@@ -7,6 +7,13 @@
 -- =============================================
 -- =============================================
 -- Zadanie 1
-CREATE TYPE dbo.P4_surname FROM NVARCHAR(50) NOT NULL
+CREATE TYPE dbo.P4_surname FROM NVARCHAR(50) NOT NULL -- Tworze własny typ danych P4_surname 
 GO
 
+ALTER TABLE [240164].[Customer] --To chyba jedyna tabela ktora zawiera LastName, chyba, że coś przeoczyłem ze zmęczenia.
+ALTER COLUMN LastName P4_surname
+-- =============================================
+-- =============================================
+-- Zadanie 2
+--Tutaj deklaruje zmienna jsonowa
+DECLARE @ProductInfo NVARCHAR(MAX) = N'[{"ProductID":717
