23dd15daa7d3a4c087a7dd9c4ac3d6b4b2e61608
Piotr <ligzer123@gmail.com>
2026-05-21 12:40:14 +0200

    Update lab-7.sql

--- lab-7.sql
+++ lab-7.sql
@@ -18,11 +18,14 @@ GO
 -- Zadanie 2
 --Tworze widok z jakas tam nazwa 
 CREATE VIEW dbo.JSONCOS AS 
-SELECT p.ProductID, p.ListPrice
-FROM SalesLT.Product p
-WHERE ProductID
+SELECT ProductID, ListPrice
+FROM SalesLT.Product
 GO
 --Tutaj deklaruje zmienna jsonowa
 DECLARE @ProductInfo NVARCHAR(MAX) = N'[{"ProductID":717, "NewPrice": 1730.13},{"ProductID":718, "NewPrice": 2003.30},{"ProductID": 720, "NewPrice": 6001.34},{"ProductID": 730, "NewPrice": 50.31},{"ProductID": 740, "NewPrice": 705.50}]'
+
+SELECT vw.ProductID, vw.ListPrice, pinfo.NewPrice, (pinfo.NewPrice - vw.ListPrice) as Diff
+FROM dbo.JSONCOS vw
+
 
 --Tworze widok z jakas tam nazwa 
