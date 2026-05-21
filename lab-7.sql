ed3b96c3d507ff63239a99d76cac1712f447b7ad
Piotr <ligzer123@gmail.com>
2026-05-21 12:26:26 +0200

    Update lab-7.sql

--- lab-7.sql
+++ lab-7.sql
@@ -20,6 +20,6 @@ GO
 DECLARE @ProductInfo NVARCHAR(MAX) = N'[{"ProductID":717, "NewPrice": 1730.13},{"ProductID":718, "NewPrice": 2003.30},{"ProductID": 720, "NewPrice": 6001.34},{"ProductID": 730, "NewPrice": 50.31},{"ProductID": 740, "NewPrice": 705.50}]'
 
 --Tworze widok z jakas tam nazwa 
-CREATE VIEW dbo.JSONCOS AS
-SELECT
-	p.ProductID,
+CREATE VIEW dbo.JSONCOS AS 
+SELECT p.ProductID, p.
+FROM SalesLT.Product p
