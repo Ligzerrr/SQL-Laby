83085cd590ac0c2ccd5390579376856a485d7007
Piotr <ligzer123@gmail.com>
2026-06-08 13:47:08 +0200

    Update lab-10.sql

--- lab-10.sql
+++ lab-10.sql
@@ -8,7 +8,7 @@
 -- =============================================
 -- Zadanie 1
 GO
---Mały problem co do tego zadania.
+--Mały problem co do tego zadania. Nie rozumiem do czego ten TIP nawiazuje? Nie pamietam tworzenia wlasnych typow danych specyficznie pod SalesLT.Product i troche mnie to? zdziwilo?
 CREATE OR ALTER PROCEDURE dbo.UpdatePrices
 	@IDProd int,
 	@NEWLPrice decimal(10,2)
