7edacfd13b3be2de5db5ef676a2e9b1ebbb31d83
Piotr <ligzer123@gmail.com>
2026-06-08 13:55:51 +0200

    Update lab-10.sql

--- lab-10.sql
+++ lab-10.sql
@@ -7,6 +7,8 @@
 -- =============================================
 -- =============================================
 -- Zadanie 1
+-- Dam taki mały prefix, dalej mam problem z no connection jeśli chodzi o bazę, więc totalnie nie mam jak sprawdzić czy to działa.
+--Znaczy zawsze mógłbym w osobnej bazie danych pracować ale nie widzę troche sensu tego ale rozumiem jeśli powinnienem tak zrobić.
 GO
 --Mały problem co do tego zadania. Nie rozumiem do czego ten TIP nawiazuje? Nie pamietam tworzenia wlasnych typow danych specyficznie pod SalesLT.Product i troche mnie to? zdziwilo?
 CREATE OR ALTER PROCEDURE dbo.UpdatePrices
@@ -34,7 +36,11 @@ AS
 BEGIN 
 	SET NOCOUNT ON
 
-	SELECT
+	SELECT FirstName, LastName, CustomerID, EmailAddress
+	FROM [240164].Customer
+	WHERE (@FNAME = FirstName OR @FNAME = Null) AND (@LNAME = LastName OR @LNAME = Null) AND
+	(@CID = 
+
 
 -- =============================================
 -- =============================================
