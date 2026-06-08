de98b88a381076cc9abe5d535bda43dbab407ac6
Piotr <ligzer123@gmail.com>
2026-06-08 17:32:11 +0200

    Update lab-11.sql

--- lab-11.sql
+++ lab-11.sql
@@ -7,12 +7,16 @@
 -- =============================================
 -- =============================================
 -- Zadanie 1
-SELECT CTG.CategoryName, MIN(PRDT.ListPrice) OVER (Partition By ProductCategoryID) as MINLP, MAX(PRDT.ListPrice) OVER (Partition By P.ProductCategoryID) as MAXLP, Count(*) OVER (Partition by P.ProductCategoryID) as VOL
+SELECT DISTINCT CTG.Name, MIN(PRDT.ListPrice) OVER (Partition By PRDT.ProductCategoryID) as MINLP, MAX(PRDT.ListPrice) OVER (Partition By PRDT.ProductCategoryID) as MAXLP, Count(*) OVER (Partition by PRDT.ProductCategoryID) as VOL
 FROM SalesLT.Product PRDT
 JOIN SalesLT.ProductCategory CTG on PRDT.ProductCategoryID = CTG.ProductCategoryID
 -- =============================================
 -- =============================================
 -- Zadanie 2
+--Stworze wlasna tabele bo latwiej bedzie mi pracowac nad tym niz tabelami z advworks.
+CREATE TABLE #ClothingSales (
+	Name nvarchar(50),
+
 -- =============================================
 -- =============================================
 -- Zadanie 3
