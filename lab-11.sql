9f8479f32ff1d95bc259df268410724c4b51aeec
Piotr <ligzer123@gmail.com>
2026-06-08 17:22:44 +0200

    Update lab-11.sql

--- lab-11.sql
+++ lab-11.sql
@@ -8,8 +8,8 @@
 -- =============================================
 -- Zadanie 1
 SELECT CTG.CategoryName, MIN(PRDT.ListPrice) OVER (Partition By ProductCategoryID) as MINLP, MAX(PRDT.ListPrice) OVER (Partition By P.ProductCategoryID) as MAXLP, Count(*) OVER (Partition by P.ProductCategoryID) as VOL
-FROM SalesLT.Product
-
+FROM SalesLT.Product PRDT
+JOIN SalesLT.ProductCategory CTG on PRDT.ProductCategoryID = CTG.ProductCategoryID
 -- =============================================
 -- =============================================
 -- Zadanie 2
